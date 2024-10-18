1cff998ba68f71b6a11db4a75ebddd38a3d17a20
joheeyong <wh2dyd@naver.com>
2024-10-18 17:32:20 +0900

    riverpod 선정

---
+++ lib/page/MainPage.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: deprecated_member_use, duplicate_ignore, empty_catches, avoid_print, unnecessary_null_comparison
+
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+//메인화면 바텀네비게이션
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  mainActivity createState() => mainActivity();
+}
+
+class mainActivity extends State<MainPage> with TickerProviderStateMixin {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
