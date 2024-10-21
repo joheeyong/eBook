5428bde329ea085a94e03c6c58381c08c56b6810
joheeyong <wh2dyd@naver.com>
2024-10-21 17:22:36 +0900

    remote adapter

--- lib/page/MainPage.dart
+++ lib/page/MainPage.dart
@@ -1,10 +1,5 @@
-// ignore_for_file: deprecated_member_use, duplicate_ignore, empty_catches, avoid_print, unnecessary_null_comparison
 
-import 'dart:async';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 //메인화면 바텀네비게이션
 class MainPage extends StatefulWidget {
